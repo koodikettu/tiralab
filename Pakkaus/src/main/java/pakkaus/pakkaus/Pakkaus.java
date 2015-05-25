ee915008cbc7c538f3ef21245ca86687ffd9cbc1
koodikettu <markku.laakso@helsinki.fi>
2015-05-25 03:19:33 +0300

    vk2 v0.3

---
+++ Pakkaus/src/main/java/pakkaus/pakkaus/Pakkaus.java
@@ -0,0 +1,105 @@
+/*
+ * Tällä hetkellä ohjelmassa on vasta hahmoteltu tiedoston lukua ja mock-pakkausalgoritmeja.
+ */
+package pakkaus.pakkaus;
+
+import pakkaus.huffmanLogiikka.HuffmanSolmu;
+import pakkaus.huffmanLogiikka.HuffmanKoodaus;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Markku
+ */
+public class Pakkaus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        int i;
+        int[] tiheystaulu;
+        String[] kooditaulu = new String[256];
+        HuffmanSolmu huffmanPuu;
+
+        String merkkijono = "this is an example for huffman encoding";
+        merkkijono = "kissa";
+        merkkijono = "aaddddbbb";
+        tiheystaulu = HuffmanKoodaus.muodostaTiheystaulu(merkkijono);
+
+        for (i = 0; i < tiheystaulu.length; i++) {
+            if (tiheystaulu[i] > 0) {
+                System.out.println(i + " (" + ((char) i) + "): " + tiheystaulu[i]);
+            }
+        }
+        System.out.println("");
+        huffmanPuu = HuffmanKoodaus.muodostaHuffmanPuu(tiheystaulu);
+        HuffmanKoodaus.muodostaKooditaulu(huffmanPuu, "", kooditaulu);
+
+        for (i = 0; i < kooditaulu.length; i++) {
+            if (kooditaulu[i] != null) {
+                System.out.println(i + " (" + ((char) i) + "): " + kooditaulu[i]);
+            }
+        }
+
+//        // TODO code application logic here
+//        String tiedostonNimi;
+//        File lahde, kohde;
+//        FileInputStream fis, fisKohde;
+//        FileOutputStream fos;
+//        char merkki;
+//
+//        int valinta;
+//        Scanner lukija = new Scanner(System.in);
+//        System.out.println("Anna pakattavan tiedoston nimi: ");
+//        tiedostonNimi = lukija.next();
+//        lahde = new File(tiedostonNimi);
+//        fis = new FileInputStream(lahde);
+//        fos = new FileOutputStream(tiedostonNimi + ".pakattu");
+//        System.out.println("Valitse käytettävä pakkausalgoritmi:");
+//        System.out.println("1. Ei pakkausta");
+//        System.out.println("2. Joka toinen merkki");
+//        valinta = lukija.nextInt();
+//        if (valinta == 1) {
+//            eiPakkausta(fis, fos);
+//        } else {
+//            jokaToinen(fis, fos);
+//        }
+//        fis.close();
+//        fos.close();
+//        fis = new FileInputStream(lahde);
+//        fisKohde = new FileInputStream(tiedostonNimi + ".pakattu");
+//        System.out.println("Lähde: " + fis.getChannel().size());
+//        System.out.println("Kohde: " + fisKohde.getChannel().size());
+//
+//
+//    }
+//
+//    public static void eiPakkausta(FileInputStream fis, FileOutputStream fos) throws Exception {
+//        while (fis.available() > 0) {
+//            fos.write(fis.read());
+//        }
+//
+//    }
+//
+//    public static void jokaToinen(FileInputStream fis, FileOutputStream fos) throws Exception {
+//        int i = 0;
+//        int b;
+//        while (fis.available() > 0) {
+//            if (i == 0) {
+//                b = fis.read();
+//                fos.write(b);
+//                i = 1;
+//
+//            } else {
+//                fis.read();
+//                i = 0;
+//
+//            }
+//        }
+    }
+
+}
---
+++ Pakkaus/src/test/java/pakkaus/huffmanLogiikka/HuffmanKoodausTest.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pakkaus.huffmanLogiikka;
+
+import java.util.PriorityQueue;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pakkaus.huffmanLogiikka.HuffmanKoodaus;
+import pakkaus.huffmanLogiikka.HuffmanSolmu;
+
+/**
+ *
+ * @author Markku
+ */
+public class HuffmanKoodausTest {
+
+    public HuffmanKoodausTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void muodostaTiheystauluTest1() {
+        String mjono = "kissa";
+        int[] taulukko;
+        taulukko = HuffmanKoodaus.muodostaTiheystaulu(mjono);
+        assertEquals(taulukko[(int) 'k'], 1);
+        assertEquals(taulukko[(int) 'i'], 1);
+        assertEquals(taulukko[(int) 's'], 2);
+        assertEquals(taulukko[(int) 'a'], 1);
+        assertEquals(taulukko[(int) 'f'], 0);
+        assertEquals(taulukko[(int) '.'], 0);
+    }
+
+    @Test
+    public void muodostaTiheystauluTest2() {
+        String mjono = "";
+        int[] taulukko;
+        taulukko = HuffmanKoodaus.muodostaTiheystaulu(mjono);
+        for (int i = 0; i < taulukko.length; i++) {
+            assertEquals(taulukko[i], 0);
+        }
+    }
+
+    @Test
+    public void muodostaPrioriteettijonoTest() {
+        String mjono = " abbddddeeeeeccc";
+        PriorityQueue<HuffmanSolmu> prjono;
+        int[] taulukko;
+        taulukko = HuffmanKoodaus.muodostaTiheystaulu(mjono);
+        prjono = HuffmanKoodaus.muodostaPrioriteettijono(taulukko);
+        assertEquals(prjono.remove().getMerkki(), ' ');
+        assertEquals(prjono.remove().getMerkki(), 'a');
+        assertEquals(prjono.remove().getMerkki(), 'b');
+        assertEquals(prjono.remove().getMerkki(), 'c');
+        assertEquals(prjono.remove().getMerkki(), 'd');
+        assertEquals(prjono.remove().getMerkki(), 'e');
+
+    }
+
+    @Test
+    public void muodostaHuffmanPuuTest() {
+        HuffmanSolmu puu;
+        String mjono = "aaddddbbb";
+        PriorityQueue<HuffmanSolmu> prjono;
+        int[] taulukko;
+        taulukko = HuffmanKoodaus.muodostaTiheystaulu(mjono);
+        puu = HuffmanKoodaus.muodostaHuffmanPuu(taulukko);
+        assertEquals(puu.getTiheys(), 9);
+        assertEquals(puu.getVasen().getTiheys(), 4);
+        assertEquals(puu.getOikea().getTiheys(), 5);
+    }
+
+    @Test
+    public void muodostaKooditauluTest() {
+        HuffmanSolmu puu;
+        String mjono = "aaddddbbb";
+        String[] kooditaulu = new String[256];
+        PriorityQueue<HuffmanSolmu> prjono;
+        int[] taulukko;
+        taulukko = HuffmanKoodaus.muodostaTiheystaulu(mjono);
+        puu = HuffmanKoodaus.muodostaHuffmanPuu(taulukko);
+        HuffmanKoodaus.muodostaKooditaulu(puu, "", kooditaulu);
+        assertEquals(kooditaulu[(int) 'a'].equals("10"), true);
+        assertEquals(kooditaulu[(int) 'b'].equals("11"), true);
+        assertEquals(kooditaulu[(int) 'd'].equals("0"), true);
+    }
+}
